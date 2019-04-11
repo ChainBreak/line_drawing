8fa3adf200af591dc7ae556f6dc2b35445ad13ca
Thomas Rowntree <thomas.rowntree@adelaide.edu.au>
2019-04-11 10:27:46 +0930

    Scribble now saves a json

--- scribble.py
+++ scribble.py
@@ -8,6 +8,7 @@ import numpy as np
 
 import paper
 import utils
+import plot_path
 
 img_debug = True
 animate = True
@@ -41,94 +42,118 @@ class Scribble():
             cv2.namedWindow("difference",0)
             cv2.namedWindow("canvas",0)
 
-        while True:
 
+        border = 5/1000 #meters
+        paper_w, paper_h = paper.size(paper_size,paper_orientation)
+        draw_h = paper_h - 2*border
+        draw_w = paper_w - 2*border
 
-            #blur difference image
-            k = 7
-            canvas_blur = cv2.GaussianBlur(canvas,(k,k),0)
-            canvas_blur = cv2.GaussianBlur(canvas_blur,(k,k),0)
-            # canvas_blur = cv2.GaussianBlur(canvas_blur,(k,k),0)
-            #calculate difference image
-            img_diff_np = cv2.subtract(canvas_blur,img_np)
+        path_list = []
 
-            # #thresh hold the to solid white
-            # mask = img_diff_np<50
-            # img_diff_np[mask] = 0
+        path_count = 0
+        point_count = 0
 
+        try:
+            while True:
+                path = []
+                path_list.append(path)
+                path_count += 1
 
-            #find the brigest pixel as a starting location
-            rand_img = img_diff_np*np.random.rand(*img_np.shape)
-            brigest = np.argmax(rand_img)
-            cv2.imshow("rand",rand_img)
-            cv2.waitKey(1)
-            y,x = np.unravel_index(brigest,img_diff_np.shape)
-            print(x,y)
-            # x,y = (0,0)
+                #blur difference image
+                k = 7
+                canvas_blur = cv2.GaussianBlur(canvas,(k,k),0)
+                canvas_blur = cv2.GaussianBlur(canvas_blur,(k,k),0)
+                # canvas_blur = cv2.GaussianBlur(canvas_blur,(k,k),0)
 
-            dx,dy = 0.,0.
-            for i in range(100):
-                kernel=11
-                step = 5
-
-
-                x = np.clip(x, 0, image_w-1)
-                y = np.clip(y, 0, image_h-1)
-                M = np.array([[1,0,kernel/2 - x], [0,1, kernel/2 - y]], dtype="float32")
-                crop = cv2.warpAffine(img_diff_np,M,(kernel,kernel))
-
-
-                #find the centroid
-                h,w = crop.shape
-                dist = np.arange(w).reshape(1,w).astype("float")
-                crop_sum = np.sum(crop)+ 10e-6
-                xc = np.sum(crop * dist )/ crop_sum
-                yc = np.sum(crop * dist.T) / crop_sum
-
-
-                #get the vector from the center of the crop to the centroid
-                new_dx = xc - kernel//2
-                new_dy = yc - kernel//2
-
-                #get the length of the vector
-                l = math.sqrt(new_dx**2 + new_dy**2) + 10e-6
-
-                new_dx *= step/l
-                new_dy *= step/l
-
-                last_x,last_y = x,y
-
-                alpha = 0.05
-                dx += alpha*(new_dx - dx)
-                dy += alpha*(new_dy - dy)
-
-                x += dx
-                y += dy
-
-                x = np.clip(x, 0, image_w-1)
-                y = np.clip(y, 0, image_h-1)
-
-
-                cv2.line(img_diff_np,(int(round(last_x)),int(round(last_y))),(int(round(x)),int(round(y))),0)
-                cv2.line(canvas,(int(round(last_x)),int(round(last_y))),(int(round(x)),int(round(y))),0)
-
-                #normalise the vector then scale by the step speed
-
-                # print(xc,yc,dx,dy,l)
-
-
-                if animate:
-                    cv2.imshow("crop",(crop).astype("uint8"))
-                    cv2.imshow("difference",img_diff_np)
-                    cv2.imshow("canvas",canvas)
-                    cv2.waitKey(1)
+                #calculate difference image
+                img_diff_np = cv2.subtract(canvas_blur,img_np)
 
 
 
+                #find the brigest pixel as a starting location
+                rand_img = img_diff_np*np.random.rand(*img_np.shape)
+                brigest = np.argmax(rand_img)
+                y,x = np.unravel_index(brigest,img_diff_np.shape)
+
+
+                dx,dy = 0.,0.
+                for i in range(100):
 
 
 
-        if img_debug: cv2.waitKey(100)
+                    kernel=11
+                    step = 5
+
+
+                    x = np.clip(x, 0, image_w-1)
+                    y = np.clip(y, 0, image_h-1)
+                    M = np.array([[1,0,kernel/2 - x], [0,1, kernel/2 - y]], dtype="float32")
+                    crop = cv2.warpAffine(img_diff_np,M,(kernel,kernel))
+
+
+                    #find the centroid
+                    h,w = crop.shape
+                    dist = np.arange(w).reshape(1,w).astype("float")
+                    crop_sum = np.sum(crop)+ 10e-6
+                    xc = np.sum(crop * dist )/ crop_sum
+                    yc = np.sum(crop * dist.T) / crop_sum
+
+
+                    #get the vector from the center of the crop to the centroid
+                    new_dx = xc - kernel//2
+                    new_dy = yc - kernel//2
+
+                    #get the length of the vector
+                    l = math.sqrt(new_dx**2 + new_dy**2) + 10e-6
+
+                    new_dx *= step/l
+                    new_dy *= step/l
+
+                    last_x,last_y = x,y
+
+                    alpha = 0.05
+                    dx += alpha*(new_dx - dx)
+                    dy += alpha*(new_dy - dy)
+
+                    x += dx
+                    y += dy
+
+                    x = np.clip(x, 0, image_w-1)
+                    y = np.clip(y, 0, image_h-1)
+
+
+                    cv2.line(img_diff_np,(int(round(last_x)),int(round(last_y))),(int(round(x)),int(round(y))),0)
+                    cv2.line(canvas,(int(round(last_x)),int(round(last_y))),(int(round(x)),int(round(y))),0)
+
+
+                    #scale from image coordinates to paper coordinates with border
+                    x_draw = x / image_w * draw_w + border
+                    y_draw = y / image_h * draw_h + border
+
+                    #append this point to the path
+                    path.append((x_draw,y_draw))
+                    point_count += 1
+                    #normalise the vector then scale by the step speed
+
+                    # print(xc,yc,dx,dy,l)
+
+
+                    if animate:
+                        cv2.imshow("crop",(crop).astype("uint8"))
+                        cv2.imshow("difference",img_diff_np)
+                        cv2.imshow("canvas",canvas)
+                        cv2.waitKey(1)
+
+                    print(path_count,point_count)
+        except KeyboardInterrupt:
+            print("End Generation")
+
+
+        return path_list
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -141,4 +166,15 @@ if __name__ == "__main__":
 
     scribble = Scribble()
 
-    stroke_list = scribble(img_np,"A4", "portrait")
+    path_list = scribble(img_np,"A4", "portrait")
+    print("raw",utils.count_points(path_list))
+    path_list = utils.thin_path_list(path_list)
+    print("thin",utils.count_points(path_list))
+
+    output_path = ".".join(args.img_path.split(".")[:-1]) + ".json"
+    print(output_path)
+
+    with open(output_path,'w') as f:
+        json.dump(path_list,f,indent=4)
+
+    plot_path.plot(path_list)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import cv2
+import math
 
 def ensure_gray(img_np):
     #convert the image to grayscale if it is not already
@@ -36,3 +37,68 @@ def crop_resize(img_np,out_w,out_h):
     img_np = cv2.resize(img_np,(out_w,out_h))
 
     return img_np
+
+def count_points(path_list):
+    point_count = 0
+    for path in path_list:
+        point_count += len(path)
+
+    return point_count
+
+def thin_path_list(path_list):
+    for path in path_list:
+        l = len(path)
+        if l > 3:
+            old_path = path.copy()
+            path.clear()
+
+            p0 = old_path[0]
+            p2 = old_path[1]
+
+            path.append(p0)
+            for i in range(2,l):
+                p1 = p2
+                p2 = old_path[i]
+
+                #vector betweem points p0 and p1
+                v0 = [ p1[0]-p0[0], p1[1]-p0[1] ]
+                #vector betweem points p1 and p2
+                v1 = [ p2[0]-p1[0], p2[1]-p1[1] ]
+
+                angle = _cosine_angle(v0,v1)
+                angle_deg = math.degrees(angle)
+
+                if angle_deg > 10.0:
+                    path.append(p1)
+                    p0 = p1
+
+
+            path.append(p2)
+    return path_list
+
+
+
+
+def _cosine_angle(v0,v1):
+
+    #calculate the lenght of vector one
+    l0 = math.sqrt( v0[0]**2 + v0[1]**2)
+
+    #calculate the lenght of vector two
+    l1 = math.sqrt( v1[0]**2 + v1[1]**2)
+
+    #normalise vector one
+    v0[0] /= l0
+    v0[1] /= l0
+
+    #normalise vector two
+    v1[0] /= l1
+    v1[1] /= l1
+
+    #do the dot product
+    dot = v0[0]*v1[0] + v0[1]*v1[1]
+
+    #calculate the angle between the vectors in radians
+    angle = math.acos(dot)
+
+    return angle
